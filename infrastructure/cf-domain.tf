3627f14f313bc3cb034875021b075ba78cbcf893
Lorenzo Setale <lorenzo@setale.me>
2020-03-19 19:57:19 +0100

    adds a lot of comments in the code

--- infrastructure/cf-domain.tf
+++ infrastructure/cf-domain.tf
@@ -2,19 +2,18 @@
 # 	zone = "qm64.tech"
 # }
 
-# if Zone does exists:
+# Qm64 zone on cloudflare already exists, so we don't have to create a new one
 data "cloudflare_zones" "qm64_tech" {
   filter {
     name = "qm64.tech"
   }
 }
 
-
 resource "cloudflare_record" "qm64_tech_vault" {
   zone_id = lookup(data.cloudflare_zones.qm64_tech.zones[0], "id")
   name    = "vault"
   type    = "A"
-  value   = aws_instance.vault.public_ip # Manual
+  value   = aws_instance.vault.public_ip 
 
   ttl     = "1"
   proxied = true
--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -8,6 +8,11 @@ provider "aws" {
   region  = "eu-west-1"
 }
 
+# We are generating a random name for our resources, this is used to 
+# ensure that we are not reusing some resources across multiple environments
+# (ex: Production S3 bucket will differ from the development one). 
+# Consider this as a vital feature for immutable infrastructure as we should
+# not be able to differentiate machines or resources! ;-)
 resource "random_pet" "setup" {
   length = 2
   separator = "-" 
